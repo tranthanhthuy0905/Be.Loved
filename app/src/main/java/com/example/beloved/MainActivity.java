bf9d963a621ba1bc27e5852deab8b3d988274e93
tranthanhthuy0905 <tranthanhthuylhp2002@gmail.com>
2023-12-02 17:09:58 +0700

    Sign in screen

--- app/src/main/java/com/example/beloved/MainActivity.java
+++ app/src/main/java/com/example/beloved/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.getting_started);
+        setContentView(R.layout.sign_in);
     }
 }
