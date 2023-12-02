99ad7312e79259f6971de3b3d9d31455bb028227
tranthanhthuy0905 <tranthanhthuylhp2002@gmail.com>
2023-12-02 17:53:52 +0700

    Handle input and keyboard

---
+++ app/src/main/java/com/example/beloved/getStarted/SignIn.java
@@ -0,0 +1,54 @@
+package com.example.beloved.getStarted;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.beloved.R;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+
+        TextView signupQ = (TextView) findViewById(R.id.signupQ);
+        signupQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(SignIn.this, SignUp.class);
+                SignIn.this.startActivity(intent);
+                SignIn.this.finish();
+            }
+        });
+
+        Button signInBtn = (Button) findViewById(R.id.signInBtn);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Activity activity = SignIn.this;
+                View focusedView = activity.getCurrentFocus();
+                if (focusedView != null) {
+                    InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(focusedView.getWindowToken(), 0);
+                }
+                EditText username = findViewById(R.id.usernameInput);
+                EditText password = findViewById(R.id.passwordInput);
+//                Intent intent = new Intent(SignIn.this, ABC.class);  // TODO: Changing ABC by Homescreen
+//                intent.putExtra("Username", username.getText().toString());
+//                intent.putExtra("password", password.getText().toString());
+            }}
+
+        );
+    }
+}
