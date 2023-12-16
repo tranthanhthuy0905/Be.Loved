d4247b9dac18c75c45b195620a657142f5ee2822
tranthanhthuy0905 <thuytran.inspiration@gmail.com>
2023-12-16 17:32:53 +0700

    Add checkout design xml

---
+++ app/src/main/java/com/example/beloved/product/Checkout.java
@@ -0,0 +1,15 @@
+package com.example.beloved.product;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.beloved.R;
+
+public class Checkout extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkout);
+    }
+}
