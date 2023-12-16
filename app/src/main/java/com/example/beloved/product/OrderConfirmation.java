ada2daf199f55cbaf6f36efe72882e837137d28d
tranthanhthuy0905 <thuytran.inspiration@gmail.com>
2023-12-16 22:49:34 +0700

    All rating

--- app/src/main/java/com/example/beloved/product/OrderConfirmation.java
+++ app/src/main/java/com/example/beloved/product/OrderConfirmation.java
@@ -1,2 +1,29 @@
-package com.example.beloved.product;public class OrderConfirmation {
+package com.example.beloved.product;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.beloved.R;
+
+public class OrderConfirmation extends AppCompatActivity {
+
+    private TextView rate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.done_order);
+
+        rate = (TextView) findViewById(R.id.rate);
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OrderConfirmation.this, Rating.class);
+                OrderConfirmation.this.startActivity(intent);
+                OrderConfirmation.this.finish();
+            }
+        });
+    }
 }
