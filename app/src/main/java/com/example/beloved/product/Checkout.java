682bdd3acb0a0ee5d2812cc796cbaaf59892a50d
tranthanhthuy0905 <tranthanhthuylhp2002@gmail.com>
2023-12-16 23:05:16 +0700

    Update appointment

--- app/src/main/java/com/example/beloved/product/Checkout.java
+++ app/src/main/java/com/example/beloved/product/Checkout.java
@@ -1,6 +1,10 @@
 package com.example.beloved.product;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +12,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.beloved.MainActivity;
 import com.example.beloved.R;
 import com.example.beloved.adapters.ProductAdapter;
 import com.example.beloved.models.productItem;
@@ -16,6 +21,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Checkout extends AppCompatActivity {
+    private Button continueBtn;
+    private ImageView back;
     ArrayList<productItem> productList;
     ProductAdapter adapter;
     ListView orderView;
@@ -23,6 +30,14 @@ public class Checkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.checkout);
+
+        back = (ImageView) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.productListView);
         // Receive data from another screen using Intent
@@ -34,5 +49,15 @@ public class Checkout extends AppCompatActivity {
             OrderAdapter orderAdapter = new OrderAdapter(orderItemList);
             recyclerView.setAdapter(orderAdapter);
         }
+
+        continueBtn = (Button) findViewById(R.id.continueBtn);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Checkout.this, OrderConfirmation.class);
+                Checkout.this.startActivity(intent);
+                Checkout.this.finish();
+            }
+        });
     }
 }
