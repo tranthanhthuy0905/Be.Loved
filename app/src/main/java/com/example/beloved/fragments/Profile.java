05db0431692edca3628bad9ecfadd1927deb8b16
dla <dla@axon.com>
2023-12-18 01:11:20 +0700

    Merge NA-dev branch to dla/chat-new

--- app/src/main/java/com/example/beloved/fragments/Profile.java
+++ app/src/main/java/com/example/beloved/fragments/Profile.java
@@ -1,5 +1,6 @@
 package com.example.beloved.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.beloved.MainActivity;
+import com.example.beloved.user.MyProfile;
 import com.example.beloved.R;
+import com.example.beloved.databinding.FragmentProfileBinding;
+import com.example.beloved.getStarted.SignIn;
+import com.example.beloved.models.UserProfile;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Profile extends Fragment {
+    FragmentProfileBinding binding;
+
+    private FirebaseAuth auth;
+    private FirebaseDatabase db;
+    private DatabaseReference userRef;
+    protected UserProfile profile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +41,64 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        String db_url = getResources().getString(R.string.db_url);
+        db = FirebaseDatabase.getInstance(db_url);
+        userRef = db.getReference("user");
+        TextView emailView = view.findViewById(R.id.username);
+
+        Button logout = view.findViewById(R.id.logout);
+        logout.setVisibility(currentUser != null? View.VISIBLE : View.GONE);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentUser != null) {
+                    FirebaseAuth.getInstance().signOut();
+                    Intent logout = new Intent(getActivity(), MainActivity.class);
+                    startActivity(logout);
+                } else {
+                    Toast.makeText(getActivity(), "You are not signed in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        if (currentUser != null) {
+            // User is signed in
+            String email = currentUser.getEmail() != null ? currentUser.getEmail() : "";
+            String uid = currentUser.getUid() != null ? currentUser.getUid() : "";
+            emailView.setText(email);
+            if (email != null & uid != null) {
+                profile = new UserProfile(uid, email);
+            }
+        } else {
+            emailView.setText("Log in to view details");
+            emailView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(getActivity(), SignIn.class));
+                }
+            });
+        }
+
+        ImageButton viewProfile = view.findViewById(R.id.view_profileBtn);
+        viewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentUser != null) {
+                    Intent myProfile = new Intent(getActivity(), MyProfile.class);
+                    startActivity(myProfile);
+                } else {
+                    Toast.makeText(getActivity(), "You are not signed in", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/beloved/product/OrderAdapter.java
+++ app/src/main/java/com/example/beloved/product/OrderAdapter.java
@@ -1,5 +1,6 @@
 package com.example.beloved.product;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.beloved.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -34,11 +36,20 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.OrderViewHol
     @Override
     public void onBindViewHolder(@NonNull OrderViewHolder holder, int position) {
         OrderItem orderItem = orderItemList.get(position);
-
+        String imgUrl = orderItem.getImageUrl();
         // Set data to views
-        holder.productImage.setImageResource(orderItem.getImageResource());
         holder.productName.setText(orderItem.getProductName());
         holder.productPrice.setText(orderItem.getProductPrice());
+        if (imgUrl == null || imgUrl.isEmpty()) {
+            holder.productImage.setImageResource(R.drawable.placeholder);
+        } else {
+            try {
+                Picasso.get().load(imgUrl).into(holder.productImage);
+            } catch (Exception e) {
+                holder.productImage.setImageResource(R.drawable.placeholder);
+                Log.d("Cart Adapter", "cannot retrieve image for Cart");
+            }
+        }
     }
 
     @Override
